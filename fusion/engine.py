9c01fc60f2b2616bba01c52587922cbb3740f773
TheBabelDragon <sphg5nyvyr@privaterelay.appleid.com>
2026-06-23 22:31:19 -0600

    Add fusion and tracking core modules

---
+++ fusion/engine.py
@@ -0,0 +1,4 @@
+class FusionEngine:
+    def fuse(self, frame):
+        # Placeholder: Convert CSI links to voxel probability field
+        return [[0.1 for _ in range(20)] for _ in range(20)]
---
+++ tracking/tracker.py
@@ -0,0 +1,4 @@
+class TrackerEngine:
+    def update(self, voxel_field):
+        # Blob detection and tracking
+        return [{"id": 1, "centroid": (10, 10)}]
