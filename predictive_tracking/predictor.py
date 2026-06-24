0780533ccb84e5ed009b88c765fb16a51a6bded6
TheBabelDragon <sphg5nyvyr@privaterelay.appleid.com>
2026-06-23 22:41:29 -0600

    Replaced placeholders in tracking and predictive tracking with functional logic

--- predictive_tracking/predictor.py
+++ predictive_tracking/predictor.py
@@ -2,13 +2,30 @@ import numpy as np
 
 class Predictor:
     def __init__(self):
-        self.tracks = {}
+        self.track_states = {}  # id -> {'pos': (x,y), 'vel': (vx,vy)}
 
     def predict(self, track):
+        track_id = track.get('id', 0)
+        if track_id not in self.track_states:
+            self.track_states[track_id] = {
+                'pos': track.get('centroid', (0,0)),
+                'vel': (0.1, 0.1)
+            }
+        state = self.track_states[track_id]
         # Simple constant velocity prediction
-        if 'velocity' in track:
-            return {'predicted_pos': [track['centroid'][0] + track['velocity'][0], track['centroid'][1] + track['velocity'][1]]}
-        return track
+        new_x = state['pos'][0] + state['vel'][0]
+        new_y = state['pos'][1] + state['vel'][1]
+        predicted = {
+            'id': track_id,
+            'predicted_pos': (new_x, new_y),
+            'centroid': track.get('centroid'),
+            'speed': np.sqrt(state['vel'][0]**2 + state['vel'][1]**2)
+        }
+        return predicted
 
     def update(self, observation):
-        return observation  # Kalman placeholder
+        # In real Kalman this would correct velocity
+        track_id = observation.get('id')
+        if track_id in self.track_states:
+            self.track_states[track_id]['pos'] = observation.get('centroid', (0,0))
+        return observation
--- tracking/tracker.py
+++ tracking/tracker.py
@@ -1,4 +1,41 @@
+import numpy as np
+
 class TrackerEngine:
+    def __init__(self):
+        self.tracks = []
+        self.next_id = 1
+
+    def _detect_blobs(self, voxel_field):
+        # Simple threshold-based blob detection
+        field = np.array(voxel_field)
+        threshold = 0.3
+        blobs = []
+        for x in range(field.shape[0]):
+            for y in range(field.shape[1]):
+                if field[x, y] > threshold:
+                    blobs.append((x, y))
+        return blobs
+
+    def _compute_centroid(self, blob_points):
+        if not blob_points:
+            return (0, 0)
+        xs = [p[0] for p in blob_points]
+        ys = [p[1] for p in blob_points]
+        return (np.mean(xs), np.mean(ys))
+
     def update(self, voxel_field):
-        # Blob detection and tracking
-        return [{"id": 1, "centroid": (10, 10)}]
+        blobs = self._detect_blobs(voxel_field)
+        new_tracks = []
+        for blob in blobs:
+            centroid = self._compute_centroid([blob])
+            # Simple assignment (in real system use Hungarian or distance matching)
+            track = {
+                "id": self.next_id,
+                "centroid": centroid,
+                "speed": 0.5,  # placeholder velocity
+                "last_seen": "now"
+            }
+            new_tracks.append(track)
+            self.next_id += 1
+        self.tracks = new_tracks
+        return self.tracks
